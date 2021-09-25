836d866cec27967754462d53120552adf5752ca4
Vlad <vlad-kempo@yandex.ru>
2021-09-25 15:23:38 +0300

    first commit

---
+++ app/src/main/java/com/example/test3/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.test3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private SharedPreferences prefs;
+    private SharedPreferences.Editor editor;
+    private int totalCount;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        prefs = getPreferences(Context.MODE_PRIVATE);
+        editor = prefs.edit();
+
+        totalCount = prefs.getInt("counter", 0);
+        totalCount++;
+        editor.putInt("counter", totalCount);
+        editor.commit();
+
+        if(totalCount == 3) {
+            Toast.makeText(this, "Привет Влад!", Toast.LENGTH_LONG).show();
+        }
+
+
+    }
+
+
+
+}
